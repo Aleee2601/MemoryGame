f7b1190d5f96c4e0176fec3ba2ad8aa11dfc192a
ALEXANDRA\alexa <alexandraonose05@gmail.com>
2025-05-09 17:49:32 +0300

    commit

--- MemoryGame/MemoryGame/obj/Debug/net8.0-windows/MemoryGame.AssemblyInfo.cs
+++ MemoryGame/MemoryGame/obj/Debug/net8.0-windows/MemoryGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b07011131b3118977d9e9aceb91da1833e88fb06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21aa642cca92be40553495ae6f0cfce3234b05e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
